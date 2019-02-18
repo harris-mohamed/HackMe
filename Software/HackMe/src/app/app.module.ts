a1e4eee2fe38b4101c8ebdff9f71f773f427c091
amithc2 <amith.chivukula@gmail.com>
2019-02-18 14:27:00 -0600

    login page on here w nothing on it

--- Software/HackMe/src/app/app.module.ts
+++ Software/HackMe/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,

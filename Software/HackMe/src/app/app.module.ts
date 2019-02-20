2670cd3a952d97c8e059c2d8cf61c067cdf357ab
amithc2 <amith.chivukula@gmail.com>
2019-02-20 02:20:53 -0600

    bad login

--- Software/HackMe/src/app/app.module.ts
+++ Software/HackMe/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'; 
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AccountformComponent } from './accountform/accountform.component';
 
 
 @NgModule({
@@ -12,10 +14,13 @@ import { LoginComponent } from './login/login.component';
     AppComponent,
     HomeComponent,
     LoginComponent,
+    AccountformComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

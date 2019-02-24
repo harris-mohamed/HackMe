47451e718e14a7288bce2af09d5b6627a9801b7e
harris <hmoham25@illinois.edu>
2019-02-24 05:15:07 +0000

    WORKING ADC CODE MOTHER....fathers

--- Software/RPi/PiPyADC/post.py
+++ Software/RPi/PiPyADC/post.py
@@ -1,4 +1,18 @@
-import requests 
+import pyodbc
 
-#r = requests.post('http://localhost:3000', data = {'key':'value'})
-print requests.get('http://localhost:3000')
+dsn = 'src' 
+user = 'hacker@hackillinois2019'
+password = 'gOOns412' 
+database = 'HackIllinoisTest' 
+
+connString = 'DSN={0};UID={1};PWD={2};DATABASE={3};'.format(dsn,user,password,database)
+
+conn = pyodbc.connect(connString)
+
+cursor = conn.cursor()
+
+sql = "INSERT INTO SalesLT.ProductCategory (ParentProductCategoryID, Name) VALUES (?, ?)" 
+val = ("4", "AZURE CAN SUCK MY MAGNUM DONG") 
+cursor.execute(sql, val) 
+
+conn.commit()
